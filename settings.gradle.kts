de3581dbda6a3b8a0d2f279a5c761036ec030946
KingJin0060 <monkeyking0060@gmail.com>
2026-01-27 03:14:57 +0800

    Add files via upload

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,12 +10,6 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-
-    plugins {
-        id("com.android.application") version "8.3.2"
-        id("org.jetbrains.kotlin.android") version "1.9.23"
-        id("com.google.dagger.hilt.android") version "2.48"
-    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
